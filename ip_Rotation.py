57f9d3ace4f50ab683d86b2c5cadc9305da3a3f8
kingk4 <92218590+kingk4@users.noreply.github.com>
2021-12-15 22:51:26 +0500

    Add files via upload

---
+++ ip_Rotation.py
@@ -0,0 +1,33 @@
+import requests
+from itertools import cycle
+import traceback
+from lxml.html import fromstring
+def proproxies():
+    def get_proxies():
+        url = 'https://free-proxy-list.net/'
+        response = requests.get(url)
+        parser = fromstring(response.text)
+        proxies = set()
+        for i in parser.xpath('//tbody/tr')[1:5]:                                    # take proxies from list !!!
+            if i.xpath('.//td[7][contains(text(),"yes")]'):
+                #Grabbing IP and corresponding PORT
+                proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+                proxies.add(proxy)
+        return proxies
+    proxies = get_proxies()
+    print(proxies)
+    length_proxies =len(proxies)
+    proxies = get_proxies()
+    proxy_pool = cycle(proxies)
+    url = 'https://httpbin.org/ip'
+    for i in range(length_proxies):
+        proxy = next(proxy_pool)
+        #print(proxy)
+        # print("Request #%d"%i)
+        try:
+            response = requests.get(url, proxies={"http": proxy, "http_1": proxy}, timeout=3)                      # Rotataing proxies  !!!
+            print(response.json())
+        except:
+            print("Skipping. Connnection error")
+proproxies()
+
